f9fc83aa5518a583efb9b8082222579746f2de0e
zosiat <ztrela@ucsc.edu>
2026-05-16 22:06:42 -0700

    adding patrol waypoints

---
+++ Assets/Scripts/PatrolScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuardPatrol : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    private NavMeshAgent agent;
+    private int currentWaypointIndex = 0;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        if (waypoints.Length > 0)
+        {
+            agent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
+    }
+
+    void Update()
+    {
+        if (waypoints.Length == 0) return;
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            currentWaypointIndex++;
+
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+
+            agent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
+    }
+}
