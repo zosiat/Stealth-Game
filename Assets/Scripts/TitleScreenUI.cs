88ef6478b7e683c910ae2e418f90ae157aabb59d
zosiat <ztrela@ucsc.edu>
2026-06-10 00:25:38 -0700

    credits scene

--- Assets/Scripts/TitleScreenUI.cs
+++ Assets/Scripts/TitleScreenUI.cs
@@ -6,15 +6,30 @@ public class TitleScreenUI : MonoBehaviour
     [Header("Scene Names")]
     public string gameSceneName = "MapScene";
 
+    [Header("Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject creditsPanel;
+
     public void StartGame()
     {
         SceneManager.LoadScene(gameSceneName);
     }
 
+    public void OpenCredits()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit Game");
-
         Application.Quit();
     }
 }
